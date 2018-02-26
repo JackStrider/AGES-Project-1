1570d52cede54723bb5be599c927e1b8f201561d
JackStrider <rossi.william@gmail.com>
2018-02-26 15:24:19 -0600

    Dispensor added in, some scripts made

---
+++ AGES_Project_1/Assets/Scripts/BoxScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour {
+
+    public Color color = Color.black;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(color.r, color.g, color.b, color.a);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
